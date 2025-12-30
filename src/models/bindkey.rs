03a6583f4f2f8422c10046152b3997675ddaea3b
jass835 <zaieterjass@gmail.com>
2025-12-30 14:34:34 +0100

    Sauvegarde de mes modifications sur bindkey.rs avant changement de branche

--- src/models/bindkey.rs
+++ src/models/bindkey.rs
@@ -18,3 +18,55 @@ use chrono::{DateTime, Utc};
 ///
 /// SQLx utilisera automatiquement cette struct pour lire ou écrire des lignes
 /// de la table `bindkeys`.
+
+#[derive(Debug,Serialize,Deserialize,FromRow)]
+
+pub struct BindKey{
+
+/// Identifiant unique de la BindKey (clé primaire).   
+pub id: Uuid,
+
+pub user_id: Option<Uuid>,
+
+/// L'identifiant unique hardware de la BindKey.
+/// Exemple : UID généré par l'ESP32 ou par un secure element.
+pub bindkey_uid: String,
+
+pub fingerprint_hash: String,
+
+/// Clé publique de la BindKey, utilisée pour vérifier une signature
+/// dans le mécanisme challenge/response.
+pub public_key: String,
+
+/// Statut de la BindKey : ACTIVE | RESET | LOST | BROKEN
+/// Mappé sur l'ENUM SQL `bindkey_status`.
+pub status: BindKeyStatus,
+
+pub created_at: DateTime<Utc>,
+}
+
+/// Enum Rust représentant l'ENUM SQL `bindkey_status`.
+///
+/// SQLx::Type permet de convertir automatiquement :
+///
+///   Rust                   SQL
+///   ------------------     --------------------
+///   BindKeyStatus::ACTIVE → 'ACTIVE'
+///   BindKeyStatus::RESET  → 'RESET'
+///   BindKeyStatus::LOST   → 'LOST'
+///   BindKeyStatus::BROKEN → 'BROKEN'
+///
+/// rename_all = "UPPERCASE" assure que les variantes Rust (ACTIVE, RESET...)
+/// correspondent exactement aux valeurs SQL.
+#[derive(sqlx::Type, Serialize, Deserialize, Debug, Clone)]
+#[sqlx(type_name = "bindkey_status", rename_all = "UPPERCASE")]
+
+pub enum BindKeyStatus{
+    ACTIVE,
+    RESET,
+    LOST,
+    BROKEN,
+}
+
+
+
