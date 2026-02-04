b1c0e0267e36f8af08ed9ab8fb57a98fab330c6f
Marwa282003 <Marwa282003@github.com>
2026-02-04 17:00:28 +0100

    fix: allow user deletion by setting audit log FKs to SET NULL

--- migrations/20260126161659_add_audit_logs_indexes.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE INDEX IF NOT EXISTS idx_audit_logs_user_id
-    ON audit_logs(user_id);
-
-CREATE INDEX IF NOT EXISTS idx_audit_logs_action
-    ON audit_logs(action);
-
-CREATE INDEX IF NOT EXISTS idx_audit_logs_created_at
-    ON audit_logs(created_at DESC);
